a40d3e5ee4306ce7b0b8c4320e03c79e67ccb566
Priscila Borges <priscila.borges.nl@gmail.com>
2023-10-23 15:18:38 +0200

    Change skills section

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,49 +1,37 @@
 import React from "react";
 import "./skills.css";
-import UIDesign from "../../assets/ui-design.png"
-import WebDesign from "../../assets/website-design.png"
-import AppDesign from "../../assets/app-design.png"
 
 const Skills = () => {
     return (
-        <section id="skills">
-            <span className="skillTitle">About me</span>
-            <span className="skillDescription">
-                As an aspiring web developer with a unique background in HR, I bring a dynamic perspective to the world of technology. I have knowledge in JavaScript, HTML5, CSS3, and in the MERN stack (MongoDB, ExpressJS, ReactJS, and NodeJS), I am ready to embark on an exciting coding journey. <br />
-                <br />
-                Beyond my technical prowess, my soft skills in communication, adaptability, and rapid learning set me apart. I am eager to contribute and grow within an existing, collaborative team while thriving in an informal yet structured environment. I am actively seeking a role that not only allows me to expand my knowledge but also to make meaningful contributions to innovative projects. <br />
-                <br />
-                I love meeting new people and learning about new cultures! I have 6+ years of international living abroad experience - Brazil, USA and The Netherlands
-            </span>
-            <div className="skillsBars">
-                <div className="skillBar">
-                    <img src={UIDesign} alt="" className="skillBarImg" />
-                    <div className="skillBarText">
-                        <h2>Skill 1</h2>
-                        <p>skill 1 description description description description description   </p>
-                    </div>
-                </div>
+        <section id="skillsBars">
+            <div>
+                <h2 className="skillTitle">Technical Skills</h2>
+                <ul className="skillBar">
+                    <li className="skillBarItem">JavaScript</li>
+                    <li className="skillBarItem">HTML5</li>
+                    <li className="skillBarItem">CSS3</li>
+                    <li className="skillBarItem">MongoDB</li>
+                    <li className="skillBarItem">ExpressJS</li>
+                    <li className="skillBarItem">ReactJS</li>
+                    <li className="skillBarItem">NodeJS</li>
+                    <li className="skillBarItem">Git</li>
+                    <li className="skillBarItem">GitHub</li>
+                </ul>
 
-                <div className="skillBar">
-                    <img src={WebDesign}alt="" className="skillBarImg" />
-                    <div className="skillBarText">
-                        <h2>Skill 2</h2>
-                        <p>skill 2 description description description description description </p>
-                    </div>
-                </div>
-
-                <div className="skillBar">
-                    <img src={AppDesign} alt="" className="skillBarImg" />
-                    <div className="skillBarText">
-                        <h2>Skill 3</h2>
-                        <p>skill 3 description description description description description </p>
-                    </div>
-                </div>
-
-            </div>
-
+                <h2 className="skillTitle">Soft Skills</h2>
+                <ul className="skillBar">
+                    <li className="skillBarItem">Adaptability</li>
+                    <li className="skillBarItem">Communication</li>
+                    <li className="skillBarItem">Flexibility</li>
+                    <li className="skillBarItem">Empathy</li>
+                    <li className="skillBarItem">Relationship building</li>
+                    <li className="skillBarItem">Good listening</li>
+                    <li className="skillBarItem">Reliability</li>
+                    <li className="skillBarItem">Negotiating skills</li>
+                </ul>
+            </div>            
         </section>
-    )
+    );
 }
 
 export default Skills;
