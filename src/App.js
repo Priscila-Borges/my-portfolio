68c66d84ba753cd81e74e7e26451c61a34762951
Priscila Borges <priscila.borges.nl@gmail.com>
2023-10-23 15:18:09 +0200

    Add  about section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
+import About from "./components/About/about";
 import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Intro />
+      <About />
       <Skills />
       <Works />
     </div>
---
+++ src/components/About/about.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+    return (
+        <section id="about">
+            <span className="aboutTitle">About me</span>
+
+            <span className="aboutDescription">
+                As an aspiring web developer with a unique background in HR, I bring a dynamic perspective to the world of technology. I have knowledge in JavaScript, HTML5, CSS3, and in the MERN stack (MongoDB, ExpressJS, ReactJS, and NodeJS), I am ready to embark on an exciting coding journey. <br />
+                <br />
+                Beyond my technical prowess, my soft skills in communication, adaptability, and rapid learning set me apart. I am eager to contribute and grow within an existing, collaborative team while thriving in an informal yet structured environment. I am actively seeking a role that not only allows me to expand my knowledge but also to make meaningful contributions to innovative projects. <br />
+                <br />
+                I love meeting new people and learning about new cultures! I have 6+ years of international living abroad experience - Brazil, USA and The Netherlands
+            </span>           
+        </section>
+
+
+    )
+}
+
+export default About;
