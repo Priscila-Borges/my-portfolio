227b61ebef3228532182814ee036dae7d9ef0844
Priscila Borges <priscila.borges.nl@gmail.com>
2023-10-23 22:46:30 +0200

    Add projects to works section

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,23 +1,60 @@
 import React from "react";
 import "./works.css";
-import Portfolio1 from "../../assets/portfolio-1.png"
-import Portfolio2 from "../../assets/portfolio-2.png"
-import Portfolio3 from "../../assets/portfolio-3.png"
-
+import Project1Image from "../../assets/project1.png";
+import Project2Image from "../../assets/project2.png";
+import Project3Image from "../../assets/project3.png";
 
 const Works = () => {
     return (
         <section id="works">
-            <h2 className="worksTitle">My Portifolio</h2>
-            <span className="worksDescription">Nice description of all my projects</span>
+            <h2 className="worksTitle">My Projects</h2>
             <div className="workImgs">
-                <img src={Portfolio1} alt="Portfolio1" className="worksImg"/>
-                <img src={Portfolio2} alt="Portfolio2" className="worksImg"/>
-                <img src={Portfolio3} alt="Portfolio3" className="worksImg"/>
+                <div className="workImgContainer">
+                    <a href="https://globalvibes.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <img src={Project1Image} alt="Portfolio1" className="worksImg" />
+                    </a>
+                    <h3>Global Vibes</h3>
+                    <p>Web application project developed as the final assignment for a web development bootcamp. The project is a social media platform where users can share pictures related to specific hobbies.</p>
+                    <ul className="tags">
+                        <li>REACT</li>
+                        <li>BOOTSTRAP</li>
+                        <li>EXPRESS.JS</li>
+                        <li>NODE.JS</li>
+                        <li>MONGODB</li>
+                    </ul>
+                </div>
+
+                <div className="workImgContainer">
+                    <a href="https://amsterday.adaptable.app/" target="_blank" rel="noopener noreferrer">
+                        <img src={Project2Image} alt="Portfolio2" className="worksImg" />
+                    </a>
+                    <h3>amsterDay</h3>
+                    <p>The core of the application is to allow users to discover ideas for activities in Amsterdam and create their own activities.</p>
+                    <ul className="tags">
+                        <li>EXPRESS.JS</li>
+                        <li>HANDLEBARS.JS</li>
+                        <li>MONGODB</li>
+                        <li>BOOTSTRAP</li>
+                        <li>JAVASCRIPT</li>
+                        <li>NODE.JS</li>
+                    </ul>
+                </div>
+
+                <div className="workImgContainer">
+                    <a href="https://priscila-borges.github.io/oop-my-race-game/" target="_blank" rel="noopener noreferrer">
+                        <img src={Project3Image} alt="Portfolio3" className="worksImg" />
+                    </a>
+                    <h3>Race Game</h3>
+                    <p>A a simple and exciting Browser-based game where you control a character and try to jump over obstacles to achieve the highest score possible.</p>
+                    <ul className="tags">
+                        <li>JAVASCRIPT</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                    </ul>
+                </div>
             </div>
-            <button className="worksBtn">See More</button>
         </section>
     );
-}
+};
 
 export default Works;
